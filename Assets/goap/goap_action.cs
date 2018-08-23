0d5c5da1cf51a171c189b955b0d7fa8c14205941
sonil <286258386@qq.com>
2018-08-23 22:35:27 +0800

    init

---
+++ Assets/goap/goap_action.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Goap {
+	public abstract class goap_action : MonoBehaviour {
+		public abstract string[] precondition { get; }
+		public abstract string[] effects { get; }
+
+		public abstract void enter();
+
+		public abstract void exit();
+
+		public abstract void update();
+
+		public abstract bool isDone();
+
+		public abstract bool inRange();
+
+		public abstract bool isActive();
+
+		public abstract int cost();
+	}
+}
---
+++ Assets/goap/goap_fsm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Goap {
+	public class goap_fsm {
+		public goap_fsm(goap_agent agent) {
+			this.agent = agent;
+		}
+
+		private goap_agent agent;
+
+		Stack<Action<goap_fsm, goap_agent>> stackAction = new Stack<Action<goap_fsm, goap_agent>>();
+
+		public void update() {
+			if (stackAction.Peek() != null) {
+				stackAction.Peek().Invoke(this, agent);
+			}
+		}
+
+		public Action<goap_fsm, goap_agent> pop() {
+			return stackAction.Pop();
+		}
+
+		public void push(Action<goap_fsm, goap_agent> action) {
+			stackAction.Push(action);
+		}
+	}
+
+}
+
